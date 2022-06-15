e4142c4ca3e86153fef30a08efc7aebf78b71129
dhruv50ae <linkdp2012@gmail.com>
2022-06-15 19:10:25 +0100

    Set up logic for searching

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,18 @@
 import { getAllEvents } from "../../data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/eventsSearch";
+import { useRouter } from "next/router";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
