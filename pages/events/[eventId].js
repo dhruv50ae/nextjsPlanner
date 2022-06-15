ede178bb2816268e94054bb609d7ad1d5d5d9926
dhruv50ae <linkdp2012@gmail.com>
2022-06-15 18:23:14 +0100

    router set up
    the router = useRouter allows you to get the current url but adding .eventId which is in the data file, gives you the url for a certain page like the first part would be /events for query then adding .eventId gives /e1 or what's defined for eventId for that specific page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,32 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
-  return <div>EventDetailPage</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found</p>;
+  }
+  const { title, date, location, image } = event;
+  return (
+    <Fragment>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={date}
+        address={location}
+        image={image}
+        imageAlt={title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
 export default EventDetailPage;
