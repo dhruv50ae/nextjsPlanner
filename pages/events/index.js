0bbe84f30b271177f667f500dc3628b3861fdb23
dhruv50ae <linkdp2012@gmail.com>
2022-06-20 16:34:15 +0100

    Update events to get pages based on the API

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
-import { getAllEvents } from "../../data";
+import { getAllEvents } from "../../helpers/apiUtil";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/eventsSearch";
 import { useRouter } from "next/router";
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
+const AllEventsPage = ({ fetchedEvents }) => {
   const router = useRouter();
+  const events = fetchedEvents;
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
@@ -17,5 +17,15 @@ const AllEventsPage = () => {
     </>
   );
 };
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      fetchedEvents: events,
+    },
+    revalidate: 60,
+  };
+}
 
 export default AllEventsPage;
