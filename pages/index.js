d00f4b259b308a629bd995a45bfb413c9a0f3b7c
dhruv50ae <linkdp2012@gmail.com>
2022-06-23 11:32:57 +0100

    Use Head Tag

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import EventList from "../components/events/eventList";
 import { getFeaturedEvents } from "../helpers/apiUtil";
+import Head from "next/head";
 
 const HomePage = ({ featuredEvents }) => {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
